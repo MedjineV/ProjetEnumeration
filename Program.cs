c98bff5f09739ba73c1a031c4eef7dfdb0634e23
MedjineV <leilavincent26@gmail.com>
2023-06-21 12:42:11 +0200

    Premier commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetEnumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test1();
+            Test3();
+        }
+        static void Test1()
+        {
+            Console.WriteLine(CoursePrice.java.ToString());
+            Console.WriteLine((int)CoursePrice.java);
+        }
+        static void Test2()
+        {
+            Console.WriteLine(CoursePrice.cpp);
+            string str = CoursePrice.cpp.ToString();
+            int prix = (int)CoursePrice.cpp;
+            Console.WriteLine(str + "      " + prix);
+
+
+        }
+        static void Test3()
+        {
+            CoursePrice x = CoursePrice.java;
+            Console.WriteLine(x);
+
+
+
+        }
+
+
+
+
+    }
+    enum CoursePrice
+    {
+        java = 100,
+        cpp,
+        csharp = 200
+    }
+    enum civilite
+    {
+        mr,
+        mlle,
+        mme
+    }
+}
